49cabd33c8534a9421daa33a45206ef8e3ff0511
misatol8k <misatolkurihara@gmail.com>
2020-12-01 19:04:38 +0900

    アプリ名設定間違えのため修正しました

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
-module MiniBlog
+module PetitBlog
   class Application < Rails::Application
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
