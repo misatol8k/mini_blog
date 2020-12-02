6b7114cd0b7eb9d150fd346cdb5b936ee50ad053
misatol8k <misatolkurihara@gmail.com>
2020-12-02 17:37:22 +0900

    投稿機能を作成しました

--- app/controllers/mini_blogs_controller.rb
+++ app/controllers/mini_blogs_controller.rb
@@ -2,4 +2,23 @@ class MiniBlogsController < ApplicationController
   def index
     @mini_blogs = MiniBlog.all
   end
+  def new
+    @mini_blog = MiniBlog.new
+  end
+  def create
+    @mini_blog = MiniBlog.new(mini_blog_params)
+    if params[:back]
+      render :new
+    else
+      if @mini_blog.save
+        redirect_to mini_blogs_path, notice: "ブログを作成しました！"
+      else
+        render :new
+      end
+    end
+  end
+  private
+  def mini_blog_params
+    params.require(:mini_blog).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/mini_blogs', to: 'mini_blogs#index'
+  resources :mini_blogs do
+  end
 end
